951d9e626752013d5803842f58e5af15a974d3ae
Mohammed Abdul Raheem <136875676+maraheem12@users.noreply.github.com>
2025-11-10 21:21:08 +0530

    Update AboutPlatform.jsx

--- frontend/src/components/AboutPlatform.jsx
+++ frontend/src/components/AboutPlatform.jsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from 'react';
 
-// SVG Icons for Check and Cross
 const CheckIcon = () => (
   <svg className="w-6 h-6 text-green-500 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
